6e833ecef08a9ad05509d20becec5d0aafdb976a
Rofiif <Rofiif@DESKTOP-16J70UT>
2017-10-10 20:41:36 +0700

    M Rofiiful Huda

--- Algodata3D/src/SingleLinkedList/Main.java
+++ Algodata3D/src/SingleLinkedList/Main.java
@@ -11,6 +11,12 @@ package SingleLinkedList;
  */
 public class Main {
     public static void main(String[] args) {
-        
+         Chain c = new Chain();
+        c.add(0, 1);
+        c.add(1, 2);
+        c.add(2, 3);
+        System.out.println(c.toString());
+        c.remove(1);
+        System.out.println(c.toString());
     }
-}
+    }
